9d1627e02ce13eb64564cd05bff28a6cf9086630
notmike-5 <notmike-5@users.noreply.github.com>
2024-05-25 22:30:14 -0600

    removed *256.sv files, bit-size is misleading for parameterized modules

--- rtl/tb/test.sh
+++ rtl/tb/test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-iverilog -g2005-sv -o full_adder_tb full_adder_tb.sv ../add256.sv
-iverilog -g2005-sv -o full_subtractor_tb full_subtractor_tb.sv ../add256.sv
+iverilog -g2005-sv -o full_adder_tb full_adder_tb.sv ../add.sv
+iverilog -g2005-sv -o full_subtractor_tb full_subtractor_tb.sv ../add.sv
 
-iverilog -g2005-sv -o rca_tb rca_tb.sv ../add256.sv
-iverilog -g2005-sv -o cla_tb cla_tb.sv ../add256.sv
-iverilog -g2005-sv -o csa_tb csa_tb.sv ../add256.sv
+iverilog -g2005-sv -o rca_tb rca_tb.sv ../add.sv
+iverilog -g2005-sv -o cla_tb cla_tb.sv ../add.sv
+iverilog -g2005-sv -o csa_tb csa_tb.sv ../add.sv
 
-iverilog -g2005-sv -o add_sub_tb add_sub_tb.sv ../add256.sv 
+iverilog -g2005-sv -o add_sub_tb add_sub_tb.sv ../add.sv 
 
 iverilog -g2005-sv -o mult_tb mult_tb.sv ../mult.sv
-iverilog -g2005-sv -o divu_tb divu_tb.sv ../divu256.sv
-iverilog -g2005-sv -o mod_p_tb mod_p_tb.sv ../mod_p.sv ../divu256.sv
+iverilog -g2005-sv -o divu_tb divu_tb.sv ../divu.sv
+iverilog -g2005-sv -o mod_p_tb mod_p_tb.sv ../mod_p.sv ../divu.sv
 
 #printf "$?" # check success at each stage and only run successful testbenches?
 
@@ -24,10 +24,10 @@ iverilog -g2005-sv -o mod_p_tb mod_p_tb.sv ../mod_p.sv ../divu256.sv
 ./add_sub_tb
 
 # mult.sv
-#./mult_tb
+./mult_tb
 
-# divu256.sv
-#./divu_tb
+# divu.sv
+./divu_tb
 
 # mod_q.sv
 ./mod_p_tb
